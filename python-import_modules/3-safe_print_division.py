e7e6c34d6b128d844b966801e05de32da9a8c767
ChyOakley <106038457+ChyOakley@users.noreply.github.com>
2023-10-03 22:19:43 +0100

    Update 3-safe_print_division.py

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,7 +3,5 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         return None
-    except Exception as e:
-        return None
     finally:
         print("Inside result: {}".format(result))
