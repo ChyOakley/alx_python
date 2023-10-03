f7f39a41f0a5c04e0911e38c38180fd8d20f5d92
ChyOakley <106038457+ChyOakley@users.noreply.github.com>
2023-10-03 21:56:21 +0100

    Create 1-args.py

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+import sys
+
+argv = sys.argv[1:]
+
+num_args = len(argv)
+
+num_args_format = "{} arguments{}"
+arg_format = "{}: {}"
+
+if num_args == 0:
+    print(num_args_format.format(num_args, "."))
+    print(".")
+else:
+    print(num_args_format.format(num_args, ":" if num_args > 1 else ""))
+
+    for i, arg in enumerate(argv, start=1):
+        print(arg_format.format(i, arg))
