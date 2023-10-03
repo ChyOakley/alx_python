f08e31d169871def66006bd68ebc3e7f5e5f9b86
ChyOakley <106038457+ChyOakley@users.noreply.github.com>
2023-10-03 21:58:17 +0100

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -7,11 +7,12 @@ num_args = len(argv)
 num_args_format = "{} arguments{}"
 arg_format = "{}: {}"
 
-if num_args == 0:
-    print(num_args_format.format(num_args, "."))
-    print(".")
-else:
-    print(num_args_format.format(num_args, ":" if num_args > 1 else ""))
+if __name__ == "__main__":
+    if num_args == 0:
+        print(num_args_format.format(num_args, "."))
+        print(".")
+    else:
+        print(num_args_format.format(num_args, ":" if num_args > 1 else ""))
 
-    for i, arg in enumerate(argv, start=1):
-        print(arg_format.format(i, arg))
+        for i, arg in enumerate(argv, start=1):
+            print(arg_format.format(i, arg))
